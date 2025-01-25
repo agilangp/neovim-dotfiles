7ac2755f2fe0f1ef93f9ca3dc426518ddd55c148
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-01-26 02:16:39 +0700

    change colorscheme

--- lua/algilangp/plugins/colorscheme.lua
+++ lua/algilangp/plugins/colorscheme.lua
@@ -1,9 +1,18 @@
 return {
     {
-        "rebelot/kanagawa.nvim",
+        -- "rebelot/kanagawa.nvim",
+        "olimorris/onedarkpro.nvim",
         priority = 1000,
         config = function()
-            vim.cmd([[colorscheme kanagawa]])
+            require('onedarkpro').setup({
+                options = {
+                    transparency = true,
+                    cursorline = true
+                }
+
+            })
+            -- vim.cmd([[colorscheme kanagawa]])
+            vim.cmd([[colorscheme onedark_dark]])
         end,
     },
 }
