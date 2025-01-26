3ec610abab5f7fa7cceda07c479b443b66dfce33
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-01-27 00:34:00 +0700

    Finish add essential plugins

--- lua/algilangp/lazy.lua
+++ lua/algilangp/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("algilangp.plugins", {
+require("lazy").setup({{import = "algilangp.plugins"}, {import = "algilangp.plugins.lsp"}}, {
     checker = {
         enabled = true,
         notify = false,
