11cb97245e3ccf92011883712226894b177c5b71
A. Gilang P. <al.gilang.p@gmail.com>
2025-01-28 18:34:11 +0700

    Add name to which-key group

--- lua/algilangp/core/keymaps.lua
+++ lua/algilangp/core/keymaps.lua
@@ -2,24 +2,23 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<Esc>", {desc = "Exit insert mode with jk"})
-keymap.set("i", "kj", "<Esc>", {desc = "Exit insert mode with kj"})
+keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode with kj" })
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", {desc = "clear search highlights" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights" })
 
 keymap.set("n", "x", '"_x')
 
-keymap.set("n", "<leader>+", "<C-a>", {desc = "Increment number"})
-keymap.set("n", "<leader>-", "<C-x>", {desc = "Decrement number"})
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", {desc = "Split window vertically"})
-keymap.set("n", "<leader>sh", "<C-w>v", {desc = "Split window horizontally"})
-keymap.set("n", "<leader>se", "<C-w>=", {desc = "Make split equal size"})
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "Close current split"})
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>v", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {desc = "Open new tab"})
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close current tab"})
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {desc = "Go to next tab"})
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {desc = "Go to previous tab"})
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc = "Open current buffer in new tab"})
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Switch window up" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Switch window down" })
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Switch window left" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Switch window right" })
--- lua/algilangp/plugins/which-key.lua
+++ lua/algilangp/plugins/which-key.lua
@@ -1,13 +1,25 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		spec = {
+			{ "<leader>c", group = "Code action" },
+			{ "<leader>e", group = "Explorer" },
+			{ "<leader>f", group = "Telescope" },
+			{ "<leader>h", group = "Gitsigns" },
+			{ "<leader>m", group = "Format" },
+			{ "<leader>n", group = "Clear highlight" },
+			{ "<leader>r", group = "LSP" },
+			{ "<leader>s", group = "Split" },
+			{ "<leader>w", group = "Session" },
+			{ "<leader>x", group = "Trouble" },
+		},
+	},
 }
