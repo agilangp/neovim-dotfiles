19045fcc45a09b6d10bac23848fbbdb245925af8
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-02-20 09:36:43 +0700

    add: mini.pairs

--- lua/algilangp/plugins/mini.lua
+++ lua/algilangp/plugins/mini.lua
@@ -1,18 +1,30 @@
 return {
 	{
-		"echasnovski/mini.indentscope",
-		version = false,
-		config = function()
-			local indentscope = require("mini.indentscope")
-			indentscope.setup()
-		end,
-	},
-	{
 		"echasnovski/mini.ai",
-		version = false,
+		version = "*",
 		config = function()
 			local ai = require("mini.ai")
 			ai.setup()
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			local pairs = require("mini.pairs")
+			pairs.setup({
+				modes = { insert = true, command = true, terminal = false },
+				-- skip autopair when next character is one of these
+				skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+				-- skip autopair when the cursor is inside these treesitter nodes
+				skip_ts = { "string" },
+				-- skip autopair when next character is closing pair
+				-- and there are more closing pairs than opening pairs
+				skip_unbalanced = true,
+				-- better deal with markdown code blocks
+				markdown = true,
+			})
 		end,
 	},
 }
