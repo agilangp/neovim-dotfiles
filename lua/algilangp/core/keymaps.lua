e9f7337690c5f240dedf5cd29c6f9cdfb26cb837
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-05-04 13:22:06 +0700

    add: tab/buffer navigation

--- lua/algilangp/core/keymaps.lua
+++ lua/algilangp/core/keymaps.lua
@@ -21,5 +21,7 @@ keymap.set("n", "<C-h>", "<C-w>h", { desc = "Switch window left" })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Switch window right" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tn", "<cmd>bn<CR>", { desc = "Go to next tab" }) -- close current tab
+keymap.set("n", "<leader>tl", "<cmd>bp<CR>", { desc = "Go to previous tab" }) -- close current tab
 keymap.set("n", "<leader>tx", "<cmd>bd<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
