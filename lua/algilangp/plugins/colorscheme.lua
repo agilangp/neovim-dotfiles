c312ad43feeeace2550509802ffd2d9610b139ee
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-06-05 00:36:01 +0700

    change colorscheme to oxocarbon

--- lua/algilangp/plugins/colorscheme.lua
+++ lua/algilangp/plugins/colorscheme.lua
@@ -1,17 +1,19 @@
 return {
 	{
 		-- "rebelot/kanagawa.nvim",
-		"olimorris/onedarkpro.nvim",
+		-- "olimorris/onedarkpro.nvim",
+		"nyoom-engineering/oxocarbon.nvim",
 		priority = 1000,
 		config = function()
-			require("onedarkpro").setup({
-				options = {
-					transparency = true,
-					cursorline = true,
-				},
-			})
+			-- require("onedarkpro").setup({
+			-- 	options = {
+			-- 		transparency = true,
+			-- 		cursorline = true,
+			-- 	},
+			-- })
 			-- vim.cmd([[colorscheme kanagawa]])
-			vim.cmd([[colorscheme onedark_dark]])
+			-- vim.cmd([[colorscheme onedark_dark]])
+			vim.cmd([[colorscheme oxocarbon]])
 		end,
 	},
 }
