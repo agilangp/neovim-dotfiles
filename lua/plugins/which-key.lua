498fbec6929db71b76aa66e1b85b8dd48bb99951
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-07-28 14:54:01 +0700

    add fzf-lua and keymap

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        preset = 'helix',
+        spec = {
+            { "<leader>b", group = "Buffer" },
+            { "<leader>c", group = "Code" },
+            { "<leader>f", group = "Find" },
+            { "<leader>l", group = "LSP" },
+            { "<leader>s", group = "Split" },
+        }
+    },
+    keys = {
+        { "<leader>fb", "<cmd>FzfLua buffers<cr>",   desc = "Find Buffers" },
+        { "<leader>ff", "<cmd>FzfLua files<cr>",     desc = "Find Files" },
+        { "<leader>fs", "<cmd>FzfLua live_grep<cr>", desc = "Find Strings" }
+    }
+}
