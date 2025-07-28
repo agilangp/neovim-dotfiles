49b1b5c97f78812acf4326347ef6f9096a03cb95
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-07-28 14:51:24 +0700

    add mini plugins

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        'echasnovski/mini.ai',
+        version = '*',
+        config = function()
+            require('mini.ai').setup()
+        end
+    },
+    {
+        'echasnovski/mini.pairs',
+        version = '*',
+        config = function()
+            require('mini.pairs').setup()
+        end
+    },
+    {
+        'echasnovski/mini.surround',
+        version = '*',
+        config = function()
+            require('mini.surround').setup()
+        end
+    },
+}
