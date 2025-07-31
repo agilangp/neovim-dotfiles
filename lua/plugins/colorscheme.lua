cfc61041894330f0853c5454ed3bd9799fd9e24f
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-07-31 07:13:06 +0700

    use base16 colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,29 @@
+-- return {
+--     "scottmckendry/cyberdream.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd("colorscheme cyberdream")
+--     end,
+--
+-- }
+
+-- return {
+--     "navarasu/onedark.nvim",
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--         require('onedark').setup {
+--             style = 'darker'
+--         }
+--         -- Enable theme
+--         require('onedark').load()
+--     end
+-- }
+
+
 return {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
+    "RRethy/base16-nvim",
     config = function()
-        vim.cmd("colorscheme cyberdream")
-    end,
+        vim.cmd('colorscheme base16-onedark-dark')
+    end
 }
