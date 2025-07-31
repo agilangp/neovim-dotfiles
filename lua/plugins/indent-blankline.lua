b7d92ecc432088fa97107c3448934286a4eeca4f
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-07-31 09:37:18 +0700

    set indent to use scope

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -5,6 +5,27 @@ return {
     ---@type ibl.config
     opts = {},
     config = function()
-        require("ibl").setup()
+        require("ibl").setup(
+            {
+                indent = { char = "┊" },
+                scope = {
+                    enabled = true,          -- enables the scope lines
+                    show_start = true,       -- shows the start of the scope
+                    show_end = false,        -- optional: shows the end of the scope
+                    injected_languages = true, -- detects scopes in treesitter injected langs
+                    highlight = { "Function", "Label" }, -- you can change highlight group
+                },
+                whitespace = { remove_blankline_trail = true },
+                exclude = {
+                    filetypes = {
+                        "help",
+                        "lazy",
+                        "mason",
+                        "which-key",
+                    },
+                    buftypes = { "terminal", "nofile" },
+                },
+            }
+        )
     end
 }
