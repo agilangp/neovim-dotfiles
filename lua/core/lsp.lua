e78b29e6da8c097e3c56f843a4fe05f1944a8942
Alwin Gilang Permana <al.gilang.p@gmail.com>
2025-08-06 04:40:06 +0700

    change folder configured lsp to file configured

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -9,3 +9,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 -- vim.cmd[[set completeopt+=menuone,noselect,popup]]
 vim.diagnostic.config({ virtual_lines = { current_line = true } })
+
+vim.lsp.config('lua_ls', {
+    settings = {
+        Lua = {
+            runtime = { version = 'LuaJIT' },
+            diagnostics = { globals = { 'vim' } },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+        },
+    },
+})
+
