45763e09bfd5eb1b3eea6860f361a05ac831e769
A. Gilang P. <al.gilang.p@gmail.com>
2025-09-10 00:24:22 +0700

    change colorscheme to adwaita

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -20,10 +20,22 @@
 --     end
 -- }
 
+-- return {
+--     "RRethy/base16-nvim",
+--     config = function()
+--         vim.cmd('colorscheme base16-onedark-dark')
+--     end
+-- }
 
 return {
-    "RRethy/base16-nvim",
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+    -- configure and set on startup
     config = function()
-        vim.cmd('colorscheme base16-onedark-dark')
+        vim.g.adwaita_darker = true             -- for darker version
+        vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+        -- vim.g.adwaita_transparent = true        -- makes the background transparent
+        vim.cmd('colorscheme adwaita')
     end
 }
