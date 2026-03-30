775fd69a64a1eb97ac360b39537d84f0d7fc4295
A. Gilang P. <al.gilang.p@gmail.com>
2026-03-30 08:41:49 +0700

    change colorscheme to material dark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,4 @@
 -- return {
---     "scottmckendry/cyberdream.nvim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         vim.cmd("colorscheme cyberdream")
---     end,
---
--- }
-
--- return {
 --     "navarasu/onedark.nvim",
 --     priority = 1000, -- make sure to load this before all the other start plugins
 --     config = function()
@@ -23,7 +13,9 @@
 return {
     "RRethy/base16-nvim",
     config = function()
+        -- vim.cmd("colorscheme base16-nord")
         vim.cmd("colorscheme base16-material-darker")
+        -- vim.cmd("colorscheme base16-ashes")
     end,
 }
 
@@ -62,5 +54,14 @@ return {
 --     priority = 1000, -- Ensure it loads first
 --     config = function()
 --         vim.cmd("colorscheme onedark_dark")
+--     end,
+-- }
+
+-- return {
+--     "AlexvZyl/nordic.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("nordic").load()
 --     end,
 -- }
