227b57a4b58dbb3e29834bce939940e8cc04290c
A. Gilang P. <al.gilang.p@gmail.com>
2026-04-13 15:36:56 +0700

    Update nvim-treesitter.lua

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -28,7 +28,8 @@ return {
         vim.api.nvim_create_autocmd("FileType", {
             pattern = { "<filetype>" },
             callback = function()
-                vim.treesitter.start()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
             end,
         })
     end,
